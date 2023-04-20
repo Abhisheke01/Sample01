1678fbb0d9659d092dc376c9a01f35e827eb98a5
Abhisheke01 <naiduabhishek01@outlook.com>
2023-04-20 09:42:43 +0530

    HI

---
+++ src/App.js
@@ -0,0 +1,69 @@
+
+// Imports
+import React from "react";
+import './App.css';
+class App extends React.Component {
+
+	// Constructor
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			items: [],
+			DataisLoaded: false
+		};
+	}
+
+	// ComponentDidMount is used to
+	// execute the code
+	componentDidMount() {
+		fetch(
+"https://smaple.onrender.com/patients")
+			.then((res) => res.json())
+			.then((json) => {
+				this.setState({
+					items: json,
+					DataisLoaded: true
+				});
+			})
+	}
+	render() {
+		const { DataisLoaded, items } = this.state;
+		if (!DataisLoaded) return <div>
+			<h1> Pleses wait some time.... </h1> </div> ;
+
+		return (
+		
+
+    <div className="App">
+      <h1>PATIENT-MANAGEMENT-SYSTEM-- ABHISHEK NAIDU - 20-CIVILA48-24</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Ward</th>
+            <th>Blood Type</th>
+            <th>Disease</th>
+          </tr>
+        </thead>
+        <tbody>
+          {items.map((item) => (
+            <tr key={item.id}>
+              <td>{item.id}</td>
+              <td>{item.name}</td>
+              <td>{item.age}</td>
+              <td>{item.ward}</td>
+              <td>{item.bloodType}</td>
+              <td>{item.disease}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+	);
+}
+}
+
+export default App;
